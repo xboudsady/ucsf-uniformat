6fbe2078e45630dcb2a626d6c1716c9df011faad
Xayaseth Boudsady <boudsadyx@gmail.com>
2018-02-26 15:19:37 -0800

    Update

--- script/main.js
+++ script/main.js
@@ -1,21 +1,5 @@
-const left = document.querySelector('.left');
-const right = document.querySelector('.right');
-const container = document.querySelector('.container');
-
-
-left.addEventListener('mouseenter', () => {
-    container.classList.add('hover-left');
+$(document).ready(function() {
+    $('#class-a').click(function () {
+        $('#class-a-list').slideToggle('slow');
+    });
 });
-
-left.addEventListener('mouseleave', () => {
-    container.classList.remove('hover-left');
-});
-
-right.addEventListener('mouseenter', () => {
-    container.classList.add('hover-right');
-});
-
-right.addEventListener('mouseleave', () => {
-    container.classList.remove('hover-right');
-});
-
